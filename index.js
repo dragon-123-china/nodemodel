ac03893cbc2e86efc65438ce352d1124843520d5
dragon-123-china <58315755+dragon-123-china@users.noreply.github.com>
2020-05-09 13:19:16 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const koa = require('koa');
+const Router = require('koa-router');
 
 const app = new koa();
+const router = new Router();
 
-
-app.use(async (ctx)=>{
-    ctx.body = 'hallo 玩卡莎的丁师傅'
+router.get('/', async (ctx) => {
+    ctx.body = 'hello 卡莎丁'
 })
-
+app.use(async (ctx, next)=> {
+    ctx.set('Access-Control-Allow-Origin', '*');
+    ctx.set('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
+    ctx.set('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
+    if (ctx.method == 'OPTIONS') {
+        ctx.body = 200;
+    } else {
+        await next();
+    }
+});
+app.use(router.routes())
+app.use(router.allowedMethods())
 
 app.listen(3000);
